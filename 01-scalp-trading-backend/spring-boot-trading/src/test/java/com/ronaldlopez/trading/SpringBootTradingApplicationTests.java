480e12dab33a29a2f63fc4ec4e684538bb88aa98
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-07-03 15:46:03 -0700

    Adding Front end

---
+++ 01-scalp-trading-backend/spring-boot-trading/src/test/java/com/ronaldlopez/trading/SpringBootTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronaldlopez.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
